ccf5b24c0914a1dd4d665aa9bd2c4c54abf07f45
Dmitriy Tonkushin <DTonkushin@alfabank.ru>
2022-09-06 12:28:00 +0500

    Added simple test

---
+++ src/test/java/ru/workshop/api/controllers/RestApiControllerTest.java
@@ -0,0 +1,50 @@
+package ru.workshop.api.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import ru.workshop.api.models.Book;
+
+@WebFluxTest({RestApiController.class})
+class RestApiControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBooks(@Autowired WebTestClient client) {
+        Iterable<Book> books = client.get()
+                .uri("/books")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Book.class)
+                .returnResult()
+                .getResponseBody();
+
+        assert books.iterator().hasNext();
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void postBook() {
+    }
+
+    @Test
+    void putBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+}
